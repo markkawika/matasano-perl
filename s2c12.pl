23a1b8d08ac0205e8d004cf28a138a4da2e058d1
Mark K. Pettit <mpettit@twitter.com>
2014-12-09 14:14:14 -0800

    Completely change the algorithm for decrypting the crypttext. Instead of a gigantic prefix that is the same length as the hidden message, instead i use a single-block prefix and shrink that over and over. And I look further into the encrypted message to find the block I want to compare.

--- s2c12.pl
+++ s2c12.pl
@@ -33,8 +33,6 @@ my $block_size = discover_block_size();
 croak 'Unable to determine block size'
   if ($block_size == -1);
 
-my %dictionary = ();
-
 my @hidden_message = s2c12_encrypt([]);
 my $max_length = scalar @hidden_message;
 my $hidden_message_length = undef;
@@ -54,77 +52,35 @@ if (! defined($hidden_message_length)) {
   croak 'How can this be possible? Unable to detect message length';
 }
 
-# Integer division is in use -- no FP division.
-my $num_starting_blocks;
-{
-  use integer;
-  # if HML is 139, and blocksize is 16, NSB becomes (139 / 16) + 1 == 9
-  $num_starting_blocks = ($hidden_message_length / $block_size) + 1;
-}
-# starting_size becomes 16 * 9 - 1 == 143
-my $starting_size = ($block_size * $num_starting_blocks) - 1;
-# BOI == Block Of Interest
-# BOI becomes 9 - 1 == 8
-my $BOI = $num_starting_blocks - 1;
-# boi_start becomes 8 * 16 = 128
-my $boi_start = $BOI * $block_size;
-# boi_end becomes 128 + (16 - 1) == 128 + 15 == 143
-my $boi_end = $boi_start + ($block_size - 1);
-my @decoder = (ord('a')) x $starting_size;
-my $decoded_message = q();
-print "Here is the decoded message:\n";
-our $OUTPUT_AUTOFLUSH = 1;
-# Example of how this works:
-# 
-# Example block_size of 8, HML of 15. Derived values:
-# $hidden_message:      "X, X, X, ..."
-# $num_starting_blocks: 2
-# $starting_size:      15
-# $BOI:                 1
-# $boi_start:           8
-# $boi_end:             15
-# @decoder:
-# ( 97 97 97 97 97 97 97 97
-#   97 97 97 97 97 97 97 )
-# If we just encrypt @decoder, the ptext first becomes:
-# ( 97 97 97 97 97 97 97 97
-#   97 97 97 97 97 97 97  X
-#    X  X  X  X  X  X  X  X
-#    X  X  X  X  X  X  X  1 )
-# (Note the PKCS#7 padding at the end)
-#
-# Then, I build a dictionary by appending 0 .. 255 onto the end of @decoder
-# and encrypting. This allows me to build a dictionary of the ctext values
-# for:
-# ( 97 97 97 97 97 97 97 0 )
-# ( 97 97 97 97 97 97 97 1 )
-# ( 97 97 97 97 97 97 97 2 )
-# ...
-# ( 97 97 97 97 97 97 97 255 )
-# 
-# Then, I encrypt @decoder. Then I can compare the encrypted block value of
-# ( 97 97 97 97 97 97 97 X )
-# to all of my dictionaries to determine the value of X.
-
 my %freq;
 for my $i (0 .. 255) {
   $freq{$i} = 0;
 }
-for my $i (ord('a') .. ord('z'), ord('A') .. ord('Z'), ord(q{ })) {
+for my $i (ord('a') .. ord('z'), ord('A') .. ord('Z'), ord('0') .. ord('9'), ord(q{ })) {
   $freq{$i} = 1;
 }
 
-for (my $i = 0; $i < $hidden_message_length; $i++) {
-  my %dictionary = ();
-  my @ctext = s2c12_encrypt([ @decoder[ 0 .. (($starting_size - $i) - 1) ] ]);
+my $fill_size = $block_size - 1;
+my @fill = (ord('A')) x $fill_size;
+our $OUTPUT_AUTOFLUSH = 1;
+for my $decoder_index (0 .. ($hidden_message_length - 1)) {
+  # This encrypts using a short "fill" value, to determine the actual
+  # encrypted text of the hidden message.
+  my @ctext = s2c12_encrypt([ @fill[0 .. ($fill_size - 1)] ]);
+  my $target_block = int($decoder_index / $block_size);
+  my $block_start = $target_block * $block_size;
+  my $block_end = $block_start + $block_size - 1;
+  my $ctext_str = int_array_to_string(@ctext[$block_start .. $block_end]);
   my $decoded_character = undef;
-  for my $x (reverse sort { $freq{$a} <=> $freq{$b} } (0 .. 255)) {
-    my $boi_str = int_array_to_string(@ctext[$boi_start .. $boi_end]);
-    my @test_ctext = s2c12_encrypt([ @decoder, $x ]);
-    my $test_str = int_array_to_string(@test_ctext[$boi_start .. $boi_end]);
-    if ($test_str eq $boi_str) {
-      $freq{$x}++;
-      $decoded_character = $x;
+  # The following loop tests all possible values between 0 and 255, preferring
+  # what it's seen before (pre-seeded with alphanumerics). Once it finds a
+  # match, the loop exits and $decoded_character contains the result.
+  for my $test_char (reverse sort { $freq{$a} <=> $freq{$b} } (0 .. 255)) {
+    my @test_ctext = s2c12_encrypt([ @fill, $test_char ]);
+    my $test_str = int_array_to_string(@test_ctext[0 .. ($block_size - 1)]);
+    if ($test_str eq $ctext_str) {
+      $freq{$test_char}++;
+      $decoded_character = $test_char;
       last;
     }
   }
@@ -132,7 +88,7 @@ for (my $i = 0; $i < $hidden_message_length; $i++) {
     croak 'Unable to decode a character';
   }
   print chr $decoded_character;
-  shift @decoder;
-  push @decoder, $decoded_character;
-  $decoded_message .= chr($decoded_character);
+  $fill_size = $fill_size ? $fill_size - 1 : $block_size - 1;
+  shift @fill;
+  push @fill, $decoded_character;
 }
